17365c8fc981b4b51e384f2c13d097b995516de6
yeemi routh <yeemirouth@gmail.com>
2025-05-30 18:45:19 +1000

    clyde menu now auto updates

---
+++ ClydeInjector/ClydeUpdater.cs
@@ -0,0 +1,102 @@
+﻿namespace ClydeInjector;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+internal class ClydeUpdater
+{
+    private static string ToVersionString(uint version)
+    {
+        var major = (byte)((version >> 24) & 0xFF);
+        var minor = (byte)((version >> 16) & 0xFF);
+        var debug = (byte)((version >> 8) & 0xFF);
+
+        var result = $"v{major}.{minor}";
+        if (debug != 0)
+            result += $".{debug}";
+
+        return result;
+    }
+
+    public static readonly string TargetUri = "https://raw.githubusercontent.com/Laamy/ClydeMenu/refs/heads/master/ClydeMenu/Content";
+    
+    public static DirectoryInfo injFolder = new($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.Clyde");
+
+    public static string[] dependencies = ["0Harmony.dll"];
+    public static string[] coreLibs = ["ClydeMenu.dll", "Hot-reload.dll"];
+
+    public static void ValidateCache()
+    {
+        if (!injFolder.Exists)
+            injFolder.Create();
+
+        var client = new WebClient();
+
+        var harmony = Path.Combine(injFolder.FullName, "0Harmony.dll");
+        if (!File.Exists(harmony))
+        {
+            client.DownloadFile($"{TargetUri}/dependencies/0Harmony.dll", harmony);
+            Program.Log("Downloaded client dependencies");
+        }
+
+        var version = LatestFolder();
+        var versionPath = $"{injFolder.FullName}/version.txt";
+        if (File.Exists(versionPath))
+        {
+            if (File.ReadAllText(versionPath) == version)
+            {
+                var exit = true;
+                foreach (var lib in coreLibs)
+                    if (!File.Exists($"{injFolder.FullName}/{lib}"))
+                        exit = false;
+                if (exit)
+                {
+                    Program.Log("Clyde is up to date");
+                    return;
+                }
+            }
+        }
+
+        Program.Log("Attempting to update/repair clydemenu");
+
+        var i = 1;
+        foreach (var lib in coreLibs)
+        {
+            var targetUri = $"{TargetUri}/{version}/{lib}";
+            var target = $"{injFolder.FullName}/{lib}";
+            if (File.Exists(target))
+                File.Delete(target);
+
+            client.DownloadFile(targetUri , target);
+            Program.Log($"Successfully downloaded {lib} ({i}/{coreLibs.Length})");
+            i++;
+        }
+
+        if (File.Exists(versionPath))
+            File.Delete(versionPath);
+        File.WriteAllText(versionPath, version);
+    }
+
+    private static string LatestFolder()
+    {
+        var client = new WebClient();
+        var notes = client.DownloadString($"{TargetUri}/NOTES.cs");
+
+        foreach (var note in notes.Split('\r'))
+        {
+            if (note.Contains("//latest"))
+            {
+                var pattern = Regex.Match(note, @"0x[0-9A-Fa-f]+");
+                if (!pattern.Success)
+                    throw new Exception("Fatal error trying to get notes for clyde");
+
+                uint versionId = Convert.ToUInt32(pattern.Value, 16);
+                var version = ToVersionString(versionId);
+                return version;
+            }
+        }
+        return null;
+    }
+}
--- ClydeInjector/Program.cs
+++ ClydeInjector/Program.cs
@@ -1,33 +1,30 @@
 ﻿namespace ClydeInjector;
 
 using System;
+using System.Diagnostics;
 using System.IO;
-
+using System.Threading;
 using SharpMonoInjector;
 
 internal class Program
 {
-    static DirectoryInfo injFolder = new($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.Clyde");
+    internal static void Log(string v) => Console.WriteLine($"[ClydeInjector] {v}");
 
-    public static string[] injectables = ["ClydeMenu.dll", "0Harmony.dll"];
-    
     static void Main(string[] args)
     {
-        if (!injFolder.Exists)
-            injFolder.Create();
-
-        foreach (var injectable in injectables)
+        if (Process.GetProcessesByName("REPO").Length == 0)
         {
-            var path = Path.Combine(injFolder.FullName, injectable);
-            if (!File.Exists(path))
-                File.Copy(injectable, path);
+            Log("Please open REPO before attempting to inject clyde");
+            Thread.Sleep(3000);
+            return;
         }
 
-        Console.WriteLine($"Inject envrionment pushed to {injFolder.FullName}");
+        Log("Attempting to validate cache..");
+        ClydeUpdater.ValidateCache();
 
         // i'll do inject crap here i guess
         //smi.exe inject -p REPO -a Hot-reload.dll -n Hot_reload -c Entry -m Load
-        var rawAssembly = File.ReadAllBytes("Hot-reload.dll");
+        var rawAssembly = File.ReadAllBytes($"{ClydeUpdater.injFolder.FullName}/Hot-reload.dll");
         var @namespace = "Hot_reload";
         var entry = "Entry";
         var entryFunc = "Load";
