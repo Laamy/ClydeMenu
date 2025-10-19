177767a0b7e9486e3632aeb646ad7115b99ef272
yeemi routh <yeemirouth@gmail.com>
2025-10-20 06:35:00 +1100

    updated for latest repo version (IM SO SORRY)

--- ClydeMenu/Content/NOTES.cs
+++ ClydeMenu/Content/NOTES.cs
@@ -98,6 +98,7 @@ class ClydeChangeEntry
     }
 }
 
+[ClydeChange("Updated for R.E.P.O v0.2.2", Release_v1_2)]
 [ClydeChange("Updated for R.E.P.O v0.1.2.37_beta", Release_v1_2)]
 [ClydeChange("Updated for R.E.P.O v0.1.2.38_beta", Release_v1_3)] // hello?? bro??
 [ClydeChange("Updated for R.E.P.O v0.1.2.42_beta", Release_v1_6_1)]
@@ -114,7 +115,8 @@ static class ClydeVersion
     public const uint Release_v1_5 = 0x01050000;
     public const uint Release_v1_6_1 = 0x01060100;
     public const uint Release_v1_6_2 = 0x01060200;
-    public const uint Release_v1_7_1 = 0x01070100;//latest
+    public const uint Release_v1_7_1 = 0x01070100;
+    public const uint Release_v1_7_2 = 0x01070200;//latest
 
     public const uint Current = Release_v1_7_1;
 
---
+++ ClydeMenu/DebugTHing.cs
@@ -0,0 +1,56 @@
+﻿namespace ClydeMenu;
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+enum UILayers
+{
+    Static, // menu background page content & everything drawn as disabled
+    Enabled, // only contains texture to draw over other disabled state teexture for stuff thats enabled
+}
+
+public class DebugTHing
+{
+    static Dictionary<UILayers, Texture2D> Layers;
+
+    static Color32 COLOUR_TRANS = new Color32(0, 0, 0, 0);
+
+    public static void Load()
+    {
+        //foreach (UILayers i in Enum.GetValues(typeof(UILayers)))
+        //{
+        //    var layer = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false)
+        //    {
+        //        filterMode = FilterMode.Point,
+        //        wrapMode = TextureWrapMode.Clamp
+        //    };
+        //
+        //    if (!layer.Reinitialize(Screen.width, Screen.height))
+        //        throw new Exception("nigger v2.59399.3425.345");// very much informative
+        //
+        //    layer.Clear(COLOUR_TRANS);
+        //
+        //    Layers[i] = layer;
+        //}
+    }
+
+    static void DrawPage1()
+    {
+        //var layer = Layers[UILayers.Static];
+
+        //layer.Clear(COLOUR_TRANS);
+
+        //layer.DrawRect(new Vector2(100, 100), new Vector2(100, 100), new Color32(255 / 2, 255 / 2, 0, 255));
+    }
+
+    static void DrawPage4()
+    {
+    
+    }
+
+    public static void OnGUI()
+    {
+    }
+}
